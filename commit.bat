de5a55d8fca0e66f3ea52b411e66129f85e7c8ef
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-03 23:05:23 +0100

    add: auto-commit batch file

---
+++ commit.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+TITLE MyMedia @abdeljalil-salhi
+COLOR 07
+:RESTART
+CLEAR || CLS
+ECHO ================================================
+ECHO /                MyMedia Commit                \
+ECHO ================================================
+SET /P message="$ ~>"
+IF "%message%"=="" GOTO RESTART
+ECHO ================================================
+git checkout dev
+git add .
+git commit -m "%message%"
+git pull https://github.com/abdeljalil-salhi/MyMedia.git
+git push -u origin dev
+ECHO ================================================
+ECHO commited: %message%
+PAUSE
+CLEAR || CLS
