9a176709b59a2527e9d138fee739ccd48cf1532f
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-03 23:09:51 +0100

    delete: Procfile, wrong place

---
+++ merge.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+TITLE MyMedia @abdeljalil-salhi
+COLOR 07
+:RESTART
+CLEAR || CLS
+ECHO ================================================
+ECHO /                MyMedia Merge                 \
+ECHO ================================================
+git merge main
+git checkout main
+git merge --no-ff dev
+git push -u origin main
+ECHO ================================================
+ECHO merged: main
+PAUSE
+CLEAR || CLS
