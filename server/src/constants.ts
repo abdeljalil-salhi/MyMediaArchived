34d5f14b597e620902a3fa0a31eeb7d367706a65
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-04 19:23:09 +0100

    Add initial code for running the HTTP Server

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,1 +1,6 @@
+import "dotenv-safe/config";
+
 export const __prod__ = process.env.NODE_ENV === "production";
+
+export const PORT = process.env.PORT;
+export const CLIENT_URL = process.env.CLIENT_URL;
