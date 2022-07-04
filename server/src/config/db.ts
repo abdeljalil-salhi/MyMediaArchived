dac39952741bb0dba5cb97f89e4e68587d7f33d8
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-04 19:01:13 +0100

    Add connection to MongoDB

---
+++ server/src/config/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+import { __prod__ } from "../constants";
+
+export const MongoConnection = () => {
+  // Connect to MongoDB
+  mongoose
+    .connect(
+      __prod__
+        ? `mongodb+srv://${process.env.PROD_DB_USER_PASS}@${process.env.PROD_DB_CLUSTER_ID}.mongodb.net/${process.env.PROD_DB_NAME}`
+        : `mongodb+srv://${process.env.DEV_DB_USER_PASS}@${process.env.DEV_DB_CLUSTER_ID}.mongodb.net/${process.env.DEV_DB_NAME}`
+    )
+    // Handle successful connection to database
+    .then(() =>
+      console.log(
+        `[+] CONNECTED >> MongoDB >> ${
+          __prod__
+            ? `prod:${process.env.PROD_DB_NAME}`
+            : `dev:${process.env.DEV_DB_NAME}`
+        }`
+      )
+    )
+    // Handle error when connecting to database
+    .catch((err) =>
+      console.error(
+        `[!] FAILED >> MongoDB >> ${
+          __prod__
+            ? `prod:${process.env.PROD_DB_NAME}`
+            : `dev:${process.env.DEV_DB_NAME}`
+        } >> ${err}`
+      )
+    );
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import "reflect-metadata";
+import "dotenv-safe/config";
+
+import { MongoConnection } from "./config/db";
+
+const main = async () => {
+  // Connect to the MongoDB database
+  MongoConnection();
+};
+
+// Bootstrap the application
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
