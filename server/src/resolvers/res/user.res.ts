e1b67598402d34c560009f2358bffb9b63037e95
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-05 15:34:24 +0100

    Add User register mutation

--- server/src/resolvers/res/user.res.ts
+++ server/src/resolvers/res/user.res.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from "type-graphql";
 
-import ErrorResponse from "./error.res";
+import { ErrorResponse } from "./error.res";
 import { User } from "../../models/User.model";
 
 @ObjectType()
@@ -19,28 +19,4 @@ export class UsersResponse {
 
   @Field(() => [User], { nullable: true })
   users: User[] | null;
-}
-
-@ObjectType()
-export class FollowResponse {
-  @Field(() => [ErrorResponse], { nullable: true })
-  errors?: ErrorResponse[] | null;
-
-  @Field(() => User, { nullable: true })
-  following: User | null;
-
-  @Field(() => User, { nullable: true })
-  followed: User | null;
-}
-
-@ObjectType()
-export class UnfollowResponse {
-  @Field(() => [ErrorResponse], { nullable: true })
-  errors?: ErrorResponse[] | null;
-
-  @Field(() => User, { nullable: true })
-  unfollowing: User | null;
-
-  @Field(() => User, { nullable: true })
-  unfollowed: User | null;
 }
