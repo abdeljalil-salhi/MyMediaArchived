e08f608ea89db906c3dafbaa21cb5460071dbfde
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-09 23:22:38 +0100

    Add the get user function for message sockets handler

--- server/src/sockets/messages/functions.ts
+++ server/src/sockets/messages/functions.ts
@@ -1,4 +1,8 @@
-import { IAddUserParams, IRemoveUserParams } from "./interfaces";
+import {
+  IAddUserParams,
+  IGetUserParams,
+  IRemoveUserParams,
+} from "./interfaces";
 
 // Add a new user to the list of users
 export const addUser = ({ userId, socketId, users }: IAddUserParams) => {
@@ -9,4 +13,9 @@ export const addUser = ({ userId, socketId, users }: IAddUserParams) => {
 // Remove a user from the list of users
 export const removeUser = ({ socketId, users }: IRemoveUserParams) => {
   users = users.filter((user) => user.socketId !== socketId);
+};
+
+// Get a user from the list of users
+export const getUser = ({ userId, users }: IGetUserParams) => {
+  return users.find((user) => user.userId === userId);
 };
