dcd15d43773ae3f08454e8f6eac4156ff7067a6d
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-09 20:03:39 +0100

    Add the Upload resolver to the index file

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,5 +1,11 @@
 import { PingResolver } from "./ping.resolver";
 import { UserResolver } from "./user.resolver";
 import { PostResolver } from "./post.resolver";
+import { UploadResolver } from "./upload.resolver";
 
-export const resolvers = [PingResolver, UserResolver, PostResolver] as const;
+export const resolvers = [
+  PingResolver,
+  UserResolver,
+  PostResolver,
+  UploadResolver,
+] as const;
