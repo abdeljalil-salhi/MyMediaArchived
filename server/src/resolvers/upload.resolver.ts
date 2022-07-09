64df9a9965a3a23518f5cef8a0cb3256eb16c954
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-09 20:05:38 +0100

    Refactor the Post Media to the Upload resolver

--- server/src/resolvers/upload.resolver.ts
+++ server/src/resolvers/upload.resolver.ts
@@ -1,6 +1,7 @@
 /*********************
  UPLOAD RESOLVER
  - profilePicture()
+ - postMedia()
 **********************/
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
 import { createWriteStream, promises } from "fs";
@@ -16,6 +17,74 @@ import { MediaResponse } from "./res/media.res";
 
 @Resolver()
 export class UploadResolver {
+  @Mutation(() => MediaResponse)
+  @UseMiddleware(isAuth)
+  public async postMedia(
+    @Arg("file", () => GraphQLUpload)
+    { createReadStream, mimetype }: Upload,
+    @Ctx() context: MyContext
+  ): Promise<MediaResponse> {
+    let fileName: string;
+    let folder: string = `${__dirname}/../../uploads/post/${context.user.id}`;
+
+    if (
+      mimetype != "image/jpg" &&
+      mimetype != "image/png" &&
+      mimetype != "image/jpeg" &&
+      mimetype != "image/gif" &&
+      mimetype != "video/mp4"
+    )
+      return {
+        errors: [
+          {
+            field: "file",
+            message: "Invalid file format",
+          },
+        ],
+      };
+
+    if (mimetype == "video/mp4") fileName = `${Date.now()}.mp4`;
+    else if (mimetype == "image/gif") fileName = `${Date.now()}.gif`;
+    else fileName = `${Date.now()}.png`;
+
+    try {
+      await promises.access(folder);
+    } catch (_err) {
+      await promises.mkdir(folder);
+    }
+
+    try {
+      await new Promise(async (resolve, reject) =>
+        createReadStream()
+          .pipe(createWriteStream(`${folder}/${fileName}`))
+          .on("finish", () => {
+            resolve(true);
+          })
+          .on("error", (_: Error) => {
+            reject(false);
+          })
+      );
+
+      return {
+        errors: [],
+        media: {
+          path: `post/${context.user.id}/${fileName}`,
+          mimetype,
+        },
+      };
+    } catch (_: unknown) {
+      return {
+        errors: [
+          {
+            field: "error",
+            message: "File not found or invalid",
+          },
+        ],
+        media: null,
+      };
+    }
+  }
+
   @Mutation(() => MediaResponse)
   @UseMiddleware(isAuth)
   public async profilePicture(
