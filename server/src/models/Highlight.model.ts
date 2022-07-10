f89f45418c9eeadf99c1c78bafbac161bbbd0e50
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-10 09:47:20 +0100

    Add the initial Highlight model schema

--- server/src/models/Highlight.model.ts
+++ server/src/models/Highlight.model.ts
@@ -1,6 +1,9 @@
-import { getModelForClass, prop as Property } from "@typegoose/typegoose";
+import { getModelForClass, prop as Property, Ref } from "@typegoose/typegoose";
 import { Schema } from "mongoose";
 import { Field, ObjectType } from "type-graphql";
+import { Story } from "./Story.model";
+
+import { User } from "./User.model";
 
 @ObjectType()
 export class Highlight {
@@ -9,9 +12,45 @@ export class Highlight {
 
   @Field(() => String)
   @Property({
+    required: [true, "User is required"],
+    ref: "User",
+    type: Schema.Types.ObjectId,
+  })
+  public user: Ref<User>;
+  @Field(() => User)
+  public userObj: User;
+
+  @Field(() => String)
+  @Property({
+    trim: true,
+    default: "Highlight",
     type: Schema.Types.String,
   })
-  public value: string;
+  public group?: string;
+
+  @Field(() => [Story])
+  @Property({
+    ref: "Story",
+    default: [],
+    type: Schema.Types.ObjectId,
+  })
+  public stories: Ref<Story>[];
+  @Field(() => [User], {
+    defaultValue: [],
+  })
+  public storiesObj: Story[];
+
+  @Field(() => Date)
+  @Property({
+    type: Schema.Types.Date,
+  })
+  public createdAt: Date;
+
+  @Field(() => Date)
+  @Property({
+    type: Schema.Types.Date,
+  })
+  public updatedAt: Date;
 }
 
 export const HighlightModel = getModelForClass<typeof Highlight>(Highlight, {
