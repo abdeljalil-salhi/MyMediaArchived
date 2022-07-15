d0b3cb9f988674cb52be8ec1e0a31562020e2db2
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-15 23:10:17 +0100

    Add the Login mutation

---
+++ client/src/graphql/mutations/login.ts
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/client";
+
+export const MUTATION_LOGIN = gql`
+  mutation Login($usernameOrEmail: String!, $password: String!) {
+    login(usernameOrEmail: $usernameOrEmail, password: $password) {
+      errors {
+        ...ErrorFragment
+      }
+      user {
+        ...UserFragment
+        followersObj {
+          ...UserFragment
+        }
+        followingObj {
+          ...UserFragment
+        }
+        closeObj {
+          ...UserFragment
+        }
+        likes
+        saved
+        highlights
+        accessToken
+      }
+    }
+  }
+`;
