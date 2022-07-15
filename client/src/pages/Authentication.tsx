90345fd37dcd7f83f7c07cf853b17e8d25769e52
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-15 23:11:40 +0100

    Add the authentication stylings and keyframes

--- client/src/pages/Authentication.tsx
+++ client/src/pages/Authentication.tsx
@@ -1,11 +1,35 @@
 import * as ReactCSSTransitionGroup from "react-addons-css-transition-group";
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { Helmet } from "react-helmet";
+
+import { Login } from "../components/login/Login";
 
 interface AuthenticationProps {}
 
 export const Authentication: FC<AuthenticationProps> = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isRegister, setIsRegister] = useState(false);
+
+  const timerRegisterRef: any = useRef(null as any);
+  const timerLoginRef: any = useRef(null as any);
+
+  const goToRegister = () => {
+    setIsLogin(false);
+    timerRegisterRef.current = setTimeout(() => setIsRegister(true), 500);
+  };
+
+  const goToLogin = () => {
+    setIsRegister(false);
+    timerLoginRef.current = setTimeout(() => setIsLogin(true), 500);
+  };
+
+  useEffect(() => {
+    return () => clearTimeout(timerRegisterRef.current);
+  }, []);
+
+  useEffect(() => {
+    return () => clearTimeout(timerLoginRef.current);
+  }, []);
 
   return (
     <>
@@ -24,7 +48,7 @@ export const Authentication: FC<AuthenticationProps> = () => {
             transitionEnterTimeout={300}
             transitionLeaveTimeout={300}
           >
-            <div>hello world</div>
+            {isLogin && <Login goToRegister={goToRegister} />}
           </ReactCSSTransitionGroup>
         </div>
       </div>
