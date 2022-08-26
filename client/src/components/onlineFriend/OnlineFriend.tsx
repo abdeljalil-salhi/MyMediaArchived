ef85ca15fcfcd719e79a940f0c7c713722aaa683
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-26 01:50:02 +0100

    Add the online friend section component

---
+++ client/src/components/onlineFriend/OnlineFriend.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+import { PU } from "../../globals";
+
+interface OnlineFriendProps {
+  user: any;
+}
+
+export const OnlineFriend: FC<OnlineFriendProps> = ({ user }) => {
+
+  return (
+    <Link
+    to={`/u/${user.username}`}
+    state={{ user }}
+    draggable={false}
+  >
+    <div className="onlineFriendContainer">
+      <div className="onlineFriendProfile">
+        <img
+          src={
+            user.profile
+              ? `${PU}${user?.profile}`
+              : `${PU}profile/noAvatar.png`
+          }
+          alt={user.username}
+          className="onlineFriendImage avatar skeleton"
+          draggable={false}
+        />
+        <span className="onlineFriendStatus"></span>
+      </div>
+      <div className="onlineFriendName noneStyle">
+        {user.fullName}
+      </div>
+    </div>
+  </Link>);
+};
