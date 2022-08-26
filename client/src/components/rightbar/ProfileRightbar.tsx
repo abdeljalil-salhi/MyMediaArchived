8ed904478d2b59e971c55b0d2bb9d7a1d32fba90
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-26 01:56:02 +0100

    Add the initial profile rightbar

---
+++ client/src/components/rightbar/ProfileRightbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface ProfileRightbarProps {
+  profile?: any;
+}
+
+export const ProfileRightbar: FC<ProfileRightbarProps> = ({ profile }) => {
+  return <div className="Container"></div>;
+};
