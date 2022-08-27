7147bed25ea416af1f48aa4778c64cb53169ceed
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-27 05:15:27 +0100

    Add the Astrological Signs SVGs

---
+++ client/src/svgs/Sagittarius.tsx
@@ -0,0 +1,25 @@
+const Icon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fillRule="evenodd"
+      imageRendering="optimizeQuality"
+      shapeRendering="geometricPrecision"
+      textRendering="geometricPrecision"
+      viewBox="0 0 8268 11692"
+      xmlSpace="preserve"
+      className="astroSVG"
+    >
+      <g transform="matrix(5.5386 0 0 5.54912 -16777.01 -24593.71)">
+        <path
+          d="M4692 4570l139 989-198 31-102-721-905 907 426 425-142 142-426-426-626 622-138-138 625-623-425-426 140-142 426 426 907-905-721-101 31-198 989 138z"
+          className="fil0"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+export default Icon;
