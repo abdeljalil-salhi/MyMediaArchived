e8b7b5e89e066a4daab631aaf92420708aca756c
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-28 21:25:22 +0100

    Handle the User Not Found case

--- client/src/pages/NotFound404.tsx
+++ client/src/pages/NotFound404.tsx
@@ -1,17 +1,23 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { KeyboardArrowLeftRounded } from "@mui/icons-material";
+import { KeyboardArrowLeftRounded, KeyboardArrowRightRounded } from "@mui/icons-material";
 
 import NotFound from "../svgs/NotFound";
 import { Topbar } from "../components/topbar/Topbar";
+import { isEmpty } from "../utils/isEmpty";
 
 interface NotFound404Props {}
 
 export const NotFound404: FC<NotFound404Props> = () => {
+  const [navigations, setNavigations] = useState<number>(1);
+
   const navigate = useNavigate();
 
   useEffect(() => {
     document.title = "MyMedia - Page Not Found";
+
+    if (!isEmpty(new URLSearchParams(window.location.search).get("user")))
+      setNavigations(2);
   }, []);
 
   return (
@@ -19,10 +25,20 @@ export const NotFound404: FC<NotFound404Props> = () => {
       <Topbar />
       <div className="notFoundContainer">
         <NotFound />
-        <span className="notFoundGoBack" onClick={() => navigate(-1)}>
+        <div className="notFoundText">
+          <h1>Page Not Found</h1>
+          <p>The page you are looking for does not exist or has been moved.</p>
+        </div>
+        <div className="notFoundNavigation">
+        <span className="notFoundGoBack" onClick={() => navigate(-navigations)}>
           <KeyboardArrowLeftRounded />
           Go back
         </span>
+        <span className="notFoundGoFeed" onClick={() => navigate("/")}>
+          Go to Feed
+          <KeyboardArrowRightRounded />
+        </span>
+        </div>
       </div>
     </>
   );
