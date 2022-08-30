1d491798a05ee7104808931ff202861615776791
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-30 03:21:39 +0100

    Comment the Backdrop component

--- client/src/components/backdrop/Backdrop.tsx
+++ client/src/components/backdrop/Backdrop.tsx
@@ -6,6 +6,10 @@ interface BackdropProps {
 }
 
 export const Backdrop: FC<BackdropProps> = ({ children, onClick }) => {
+  // The Backdrop is a simple div that covers the entire screen
+  // It is used to prevent clicking on the page behind the modal
+  // It closes the modal when clicked
+
   return (
     <div className="modalBackdrop" onClick={onClick}>
       {children}
