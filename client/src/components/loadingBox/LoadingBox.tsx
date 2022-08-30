2b835ef3104e159f7dcd8da41f28ecfe4994e653
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-08-30 04:15:35 +0100

    Comment the loading box component

--- client/src/components/loadingBox/LoadingBox.tsx
+++ client/src/components/loadingBox/LoadingBox.tsx
@@ -3,6 +3,15 @@ import { FC } from "react";
 interface LoadingBoxProps {}
 
 export const LoadingBox: FC<LoadingBoxProps> = () => {
+  // the LoadingBox component is used to display a loading box
+  //
+  // Notes:
+  // - The loading box is displayed when the query is loading
+  // - The loading box is not displayed if the query is loaded, or has an error
+  //
+  // Example:
+  // <LoadingBox />
+
   return (
     <div className="loadingBox">
       <i className="fa fa-spinner fa-spin"></i>
