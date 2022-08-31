cd4be5756273bb05f2a4652b2cfab47845358f69
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-01 00:49:16 +0100

    Add the update user ContextAPI actions

--- client/src/context/actions/auth.actions.ts
+++ client/src/context/actions/auth.actions.ts
@@ -6,6 +6,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAILURE,
   LOGOUT,
+  UPDATE_USER_START,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE,
 } from "../constants/auth.constants";
 
 export const loginStart = () => ({
@@ -36,4 +39,17 @@ export const registerFailure = () => ({
 
 export const logout = () => ({
   type: LOGOUT,
+});
+
+export const updateUserStart = () => ({
+  type: UPDATE_USER_START,
+});
+
+export const updateUserSuccess = (user: any) => ({
+  type: UPDATE_USER_SUCCESS,
+  payload: user,
+});
+
+export const updateUserFailure = () => ({
+  type: UPDATE_USER_FAILURE,
 });
