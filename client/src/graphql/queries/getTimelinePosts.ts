29b9de6199df00a8f7c9a5f88f62306353dbc082
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-10 02:03:26 +0100

    Implement fragments in queries

--- client/src/graphql/queries/getTimelinePosts.ts
+++ client/src/graphql/queries/getTimelinePosts.ts
@@ -1,5 +1,9 @@
 import { gql } from "@apollo/client";
 
+import { FRAGMENT_ERROR_RESPONSE } from "../fragments/ErrorFragment";
+import { FRAGMENT_POST } from "../fragments/PostFragment";
+import { FRAGMENT_USER } from "../fragments/UserFragment";
+
 export const QUERY_GET_TIMELINE_POSTS = gql`
   query GetTimelinePosts($userId: String!, $limit: Int!, $cursor: String) {
     getTimelinePosts(userId: $userId, limit: $limit, cursor: $cursor) {
@@ -15,4 +19,7 @@ export const QUERY_GET_TIMELINE_POSTS = gql`
       hasMore
     }
   }
+  ${FRAGMENT_ERROR_RESPONSE}
+  ${FRAGMENT_USER}
+  ${FRAGMENT_POST}
 `;
