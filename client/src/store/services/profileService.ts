fb2746674de257073732f3188e6abdfd799c6fb7
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-10 18:09:56 +0100

    Add the follow/unfollow mutations to the profile service

--- client/src/store/services/profileService.ts
+++ client/src/store/services/profileService.ts
@@ -1,3 +1,5 @@
+import { FetchResult } from "@apollo/client";
+
 import { client } from "../..";
 import { isEmpty } from "../../utils/isEmpty";
 import { GraphQLAccessToken } from "../../utils/_graphql";
@@ -13,6 +15,16 @@ import {
 
 import { MUTATION_UPDATE_USER } from "../../graphql/mutations/updateUser";
 import { QUERY_GET_PROFILE } from "../../graphql/queries/getProfile";
+import { MUTATION_FOLLOW_USER } from "../../graphql/mutations/followUser";
+import { MUTATION_UNFOLLOW_USER } from "../../graphql/mutations/unfollowUser";
+import {
+  FollowUser,
+  FollowUserVariables,
+} from "../../generated/types/FollowUser";
+import {
+  UnfollowUser,
+  UnfollowUserVariables,
+} from "../../generated/types/UnfollowUser";
 
 class profileService {
   async getProfile(
@@ -20,7 +32,11 @@ class profileService {
   ): Promise<GetProfile["getProfile"]> {
     try {
       // Send the GraphQL request to the server to get the profile data
-      const res = await client.query({
+      const res: FetchResult<
+        any,
+        Record<string, any>,
+        Record<string, any>
+      > = await client.query({
         query: QUERY_GET_PROFILE,
         variables: { username },
       });
@@ -41,7 +57,11 @@ class profileService {
   ): Promise<UpdateUser["updateUser"]> {
     try {
       // Send the GraphQL updating request to the server
-      const res = await client.mutate({
+      const res: FetchResult<
+        any,
+        Record<string, any>,
+        Record<string, any>
+      > = await client.mutate({
         mutation: MUTATION_UPDATE_USER,
         variables,
         // Pass the access token to the GraphQL context
@@ -56,6 +76,62 @@ class profileService {
     } catch (err: unknown) {
       // Handle unknown errors from the server and throw them to the caller
       throw new Error("Error in getProfileService.updateProfile: " + err);
+    }
+  }
+
+  async followUser(
+    variables: FollowUserVariables,
+    accessToken: string
+  ): Promise<FollowUser["followUser"]> {
+    try {
+      // Send the GraphQL updating request to the server
+      const res: FetchResult<
+        any,
+        Record<string, any>,
+        Record<string, any>
+      > = await client.mutate({
+        mutation: MUTATION_FOLLOW_USER,
+        variables,
+        // Pass the access token to the GraphQL context
+        context: GraphQLAccessToken(accessToken),
+      });
+
+      // Handle known errors from the server and throw them as errors to the caller
+      if (isEmpty(res) || isEmpty(res.data)) throw new Error("No data");
+
+      // If the request was successful, return the value of the updated user
+      return res.data.followUser;
+    } catch (err: unknown) {
+      // Handle unknown errors from the server and throw them to the caller
+      throw new Error("Error in getProfileService.followUser: " + err);
+    }
+  }
+
+  async unfollowUser(
+    variables: UnfollowUserVariables,
+    accessToken: string
+  ): Promise<UnfollowUser["unfollowUser"]> {
+    try {
+      // Send the GraphQL updating request to the server
+      const res: FetchResult<
+        any,
+        Record<string, any>,
+        Record<string, any>
+      > = await client.mutate({
+        mutation: MUTATION_UNFOLLOW_USER,
+        variables,
+        // Pass the access token to the GraphQL context
+        context: GraphQLAccessToken(accessToken),
+      });
+
+      // Handle known errors from the server and throw them as errors to the caller
+      if (isEmpty(res) || isEmpty(res.data)) throw new Error("No data");
+
+      // If the request was successful, return the value of the updated user
+      return res.data.unfollowUser;
+    } catch (err: unknown) {
+      // Handle unknown errors from the server and throw them to the caller
+      throw new Error("Error in getProfileService.unfollowUser: " + err);
     }
   }
 }
