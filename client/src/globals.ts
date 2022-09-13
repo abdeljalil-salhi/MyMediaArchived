f801381b3a90fe88d8605c392030349cb849bd56
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-13 01:02:52 +0100

    Add the GEO API key to environment variables and API link to global constants

--- client/src/globals.ts
+++ client/src/globals.ts
@@ -3,6 +3,8 @@ export const PU: string = process.env.REACT_APP_UPLOADS_URL;
 export const PA: string = process.env.REACT_APP_API_URL;
 export const USER: string = "mymedia:user";
 export const GEO: string = "mymedia:lgeo";
+export const GPS: string = "mymedia:lgps";
 export const IP_API: string = "http://ip-api.com/json/?fields=33292287";
+export const GEO_API: string = `https://api.opencagedata.com/geocode/v1/json?key=${process.env.REACT_APP_OPENCAGE_API}`;
 export const TRANSPARENT: string =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
