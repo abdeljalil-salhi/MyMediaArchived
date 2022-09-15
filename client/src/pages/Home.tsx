04a35456b0e5215061c8f9b502cc9eebde711cca
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-15 20:01:31 +0100

    Optimize and comment the Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,16 +8,21 @@ import { Rightbar } from "../components/rightbar/Rightbar";
 interface HomeProps {}
 
 export const Home: FC<HomeProps> = () => {
+  // The Home page is the main page of the application and it contains the Topbar, Sidebar, Feed and Rightbar components.
+  //
+  // Notes:
+  // - The Home page is displayed when the user is logged in
+  // - We scroll to the top of the document when the Home page is displayed
+
+  // The useEffect() hook is used to scroll to the top of the document when the Home page is displayed
+  // and change the title of the document to MyMedia
   useEffect(() => {
+    document.title = "MyMedia";
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-  }, []);
-
-  useEffect(() => {
-    document.title = "MyMedia";
   }, []);
 
   return (
