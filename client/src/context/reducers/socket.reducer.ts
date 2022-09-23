5e1ba08a6c1f920b8718f3b6b8d32a14d3601de6
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-23 20:01:37 +0100

    Improve the socket ContextAPI

--- client/src/context/reducers/socket.reducer.ts
+++ client/src/context/reducers/socket.reducer.ts
@@ -2,12 +2,15 @@ import { SET_USERS } from "../constants/socket.constants";
 import { SocketUser } from "../types/socket.types";
 
 type SocketState = Record<string, any>;
-type SocketAction = {
+export type SocketAction = {
   type: string;
-  payload: SocketUser[];
+  payload?: SocketUser[];
 };
 
-const SocketReducer = (state: SocketState, action: SocketAction) => {
+const SocketReducer = (
+  state: SocketState,
+  action: SocketAction
+): SocketState => {
   switch (action.type) {
     case SET_USERS:
       return {
