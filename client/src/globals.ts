5f44a9238c0a2b8882e6cd006e51bac18a527eb3
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-23 13:14:23 +0100

    Add the OPENWEATHERMAP API environment variables

--- client/src/globals.ts
+++ client/src/globals.ts
@@ -5,7 +5,9 @@ export const WS: string = process.env.REACT_APP_SOCKET_URL;
 export const USER: string = "mymedia:user";
 export const GEO: string = "mymedia:lgeo";
 export const GPS: string = "mymedia:lgps";
+export const PREFERENCES: string = "mymedia:preferences";
 export const IP_API: string = "http://ip-api.com/json/?fields=33292287";
 export const GEO_API: string = `https://api.opencagedata.com/geocode/v1/json?key=${process.env.REACT_APP_OPENCAGE_API}`;
+export const WEATHER_API: string = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_OPENWEATHERMAP_API}`;
 export const TRANSPARENT: string =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
