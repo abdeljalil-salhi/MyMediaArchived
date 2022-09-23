66784461d33fd1cda87f1f5edc4c87768d143b62
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-09-23 21:40:34 +0100

    Add compactNumber() util for big numbers

--- client/src/utils/parsers.ts
+++ client/src/utils/parsers.ts
@@ -14,3 +14,7 @@ export const dateParser = (timestamp: any) => {
 
   return newDate.toString();
 };
+
+export const compactNumber = Intl.NumberFormat(null as any, {
+  notation: "compact",
+}).format;
